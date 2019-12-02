1f39781ec9346a510b11e09245b56c40f5eb5353
Ronald Rajski <ronald.rajski@gmail.com>
2019-12-02 11:11:18 -0600

    Midmorning push

---
+++ JS/external.js
@@ -0,0 +1,10 @@
+console.log("Hello from external JavaScript");
+alert("Welcome to my website!");
+var answer =prompt("What is your favorite Color?");
+ // prompt("What is your favorite Color?")
+console.log(answer);
+// if answer(blue){
+//     return"Great, blue is my favorite color too";
+    if (answer == "blue"){
+        alert("Great, blue is my favorite color too");
+    }
