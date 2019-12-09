a54447d93d61410edc5fd43a9809d3959adb6f8d
Ronald Rajski <ronald.rajski@gmail.com>
2019-12-09 16:49:23 -0600

    arrays end of day

---
+++ JS/array-lec.js
@@ -0,0 +1,29 @@
+//
+var pets =["Toby, malu, Johnie, Lily, Samson, Jill, bubbles"];
+for (var i = 0; i<pets.length; i++){
+console.log(pets[i]);
+}
+
+var i=0;
+while ((i<pets.length; i++)){
+    console.log(pets[1]);
+}
+
+i log pets.length
+0 Toby 7
+1  princess
+2 Samson
+3 Jill
+4 Bubbles
+5 Malu
+6
+
+
+console.log()
+
+pets.forEach(function (pet, i, arr) {
+    console.log(pet);
+
+    }
+
+)
