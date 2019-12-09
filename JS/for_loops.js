4dfebe9d2c5a6a10e0940243f2773bf05c5b7a14
Ronald Rajski <ronald.rajski@gmail.com>
2019-12-09 08:52:18 -0600

    morning

--- JS/for_loops.js
+++ JS/for_loops.js
@@ -1,8 +1,19 @@
+var a  = 7;
+var b;
+for (var i = 1; i <= 10; i++){
+    b = a * i;
+        document.write('' + a + "*" + 1 + "=", + b);
+    }
+    // result += '\n';
+    // console.log(j)
+
+
+
 // var result = 'x';
-// for (var i = 0; i < 11; i++{
+// for (var i = 0; i < 11; i++){
 //     for (j = 0; j < 11; i++){
 //         if (i ===0 && j > 0){
-//             result += '[' +j+']';
+//             result += (i*j) + '';
 //         }
 //         else if(j === 0 && i > 0){
 //             result += '[' +i+']';
@@ -36,15 +47,22 @@
 //
 // console.log(result);
 
+
+
+
+
+
 // Create a for loop that uses console.log to create the output shown below.
 //     start point 100, end point 5 by 5's'
+//
+// for (var i = 100; i >=5; i--)
+// {
+//     if (i % 5 === 0) {
+//         console.log(i);
+//     }
+// }
+//
 
-for (var i=100; i >=5; i--);
-    if (i % 5 === 0) {
-    console.log(i);
-    }
-
-console.log(alertMessage"am i working")
 // for (var i=0; i <=100; i++) {
 //     if (i % 5 === 0) {
 //         console.log(i);
