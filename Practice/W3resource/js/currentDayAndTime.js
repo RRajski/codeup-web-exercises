3814fe7ff3f6138ca13a14775965077dbaad5193
Ronald Rajski <ronald.rajski@gmail.com>
2019-12-15 09:59:59 -0600

    Morning push

--- Practice/W3resource/js/currentDayAndTime.js
+++ Practice/W3resource/js/currentDayAndTime.js
@@ -1,15 +1,42 @@
 "use strict"
 
-var today = new Date();
-var day = today.getDay();
-var dayList = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+// 5. Write a simple JavaScript program to join all elements of the following array into a string. Go to the editor
+// Sample array : myColor = ["Red", "Green", "White", "Black"];
+// Expected Output :
+//     "Red,Green,White,Black"
+// "Red,Green,White,Black"
+// "Red+Green+White+Black"
 
-console.log("Today is" : "+ dayList[day] + ".");
-    var hour = today.getHours();
-    var minute = today.getMinutes();
-    var second = today.seconds();
-    var prepand = (hour >=12)? "PM " : " AM");
+ var myColor = ["Red","Green","White","Black"];
+console.log(myColor.toString());
+console.log(myColor.join());
+console.log(myColor.join('+'));
 
-if (hour===0 && prepand === 'PM') {
+// 7. Write a JavaScript program to sort the items of an array. Go to the editor
+// Sample array : var arr1 = [ 3, 8, 7, 6, 5, -4, 3, 2, 1 ];
+// Sample Output : -4,-3,1,2,3,5,6,7,8
+
+var arr1=[-3,8,7,6,5,-4,3,2,1];
+var arr2=[];
+var min=arr1[0];
+var pos;
+var max=arr1[0];
+for (i=0; i<arr1.length; i++) {
+    if (max<arr1[i]) max=arr1[i];
 }
-if (minute===0 && )
+for (var i=0;i<arr1.length;i++) {
+    for (var j = 0; j < arr1.length; j++) {
+        if (arr1[j] != "x") {
+            if (min > arr1[j]) {
+                {
+                    min = arr1[j];
+                    pos = j;
+                }
+            }
+        }
+        arr2[i] = min;
+        arr1[pos] = "x";
+        min = max;
+    }
+    console.log(arr2);
+}
