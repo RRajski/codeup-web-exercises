9b5b343a8637debe33eff4743a300385350d662e
Ronald Rajski <ronald.rajski@gmail.com>
2019-12-27 10:33:38 -0600

    finished code academy practice functions

--- Practice/codeAcadeyPro/js/code-academy-practice-functions.js
+++ Practice/codeAcadeyPro/js/code-academy-practice-functions.js
@@ -4,9 +4,27 @@
 // the person’s age, and returns the boolean true if they are 18 years old or older, and the boolean false if
 //     they are not.
 
+function canIVote(age){
+    if (age >=18){
+        return true
+    }
+    else
+        return false
+}
+console.log(canIVote(10));
+
 
 // 2.Write a function, agreeOrDisagree(), that takes in two strings, and returns 'You agree!'
 // if the two strings are the same and 'You disagree!' if the two strings are different.
+
+function agreeOrDisagree(x,y){
+    if(x === y){
+        return 'You agree'
+    }
+    else
+        return 'you disagree'
+}
+console.log(agreeOrDisagree(true,'true'));
 
 
 
@@ -20,6 +38,31 @@
 // 65-140 should return 'senior citizen'
 // If the number is less than 0 or greater than 140, the program should return 'This is not a valid age'
 
+function lifePhase(age){
+    if (age < 0 || age > 140){
+        return 'This is not a valid age'
+    }
+    else if (age <4){
+        return 'baby'
+    }
+    else if (age <13){
+        return 'child'
+    }
+    else if (age <20){
+        return 'teen'
+    }
+    else if (age < 65){
+        return 'adult'
+    }
+    else
+        return 'senior citizen'
+}
+console.log(lifePhase(141));
+console.log(lifePhase(3));
+console.log(lifePhase(9));
+console.log(lifePhase(14));
+console.log(lifePhase(22));
+console.log(lifePhase(66));
 
 // 4.  Write a function, finalGrade(). It should:
 //
@@ -33,12 +76,45 @@
 // 80-89 should return: ‘B’
 // 90-100 should return: ‘A’
 
+function finalGrade(homework, midterm, final){
+    if (homework < 0 || homework >100 || midterm < 0 || midterm > 100 || final < 0 || final >100){
+        return 'You have entered an invalid grade';
+    }
+    else if ((homework+midterm+final/3) > 89){
+        return 'A';
+    }
+    else if ((homework+midterm+final/3) > 79){
+        return 'B';
+    }
+    else if ((homework+midterm+final/3) >69){
+
+        return 'C';
+    }
+    else if ((homework+midterm+final/3) > 59){
+
+        return 'D';
+    }
+    else
+        return 'F';
+}
+
+console.log(finalGrade(-7,50, 90));
+
 // 5. Write a function, reportingForDuty(), that has two string parameters, rank and lastName,
 //     and returns a string in the following format: ‘rank lastName reporting for duty!’
 
 // function ReportingForDuty('rank','lastName'){
 //     return(rank + lastName + ' reporting for duty!');
 // }
+
+// function reportingForDuty(rank,lastName){
+//     return rank + lastName + 'reporting for duty!’
+//
+// }
+//
+// console.log(reportingForDuty(Private,Fido));
+
+
 
 
 
