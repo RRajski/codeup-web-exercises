9e4ea18c89ad1ec2159282c3fe6fe1f45ba53e0f
Ronald Rajski <ronald.rajski@gmail.com>
2020-01-06 08:57:15 -0600

    edabit nemo

--- Practice/edabit/edabit.js
+++ Practice/edabit/edabit.js
@@ -142,10 +142,35 @@ function toStr(int) {
 
 }
 
-toInt("77") ➞ 77
+// toInt("77") ➞ 77
+//
+// toInt("532") ➞ 532
+//
+// toStr(77) ➞ "77"
+//
+// toStr(532) ➞ "532"
 
-toInt("532") ➞ 532
+// You're given a string of words. You need to find the word "Nemo", and return a string like this:' +
+// ' "I found Nemo at [the order of the word you find nemo]!".
+//
+// If you can't find Nemo, return "I can't find Nemo :(".
+//
+// Examples
+// findNemo("I am finding Nemo !") ➞ "I found Nemo at 4!"
+//
+// findNemo("Nemo is me") ➞ "I found Nemo at 1!"
+//
+// findNemo("I Nemo am") ➞ "I found Nemo at 2!"
+// Notes
+// ! , ? . are always separated from the last word.
+//     Nemo will always look like Nemo, and not NeMo or other capital variations.
+//     Nemo's, or anything that says Nemo with something behind it, doesn't count as Finding Nemo.
+//     If there are multiple Nemo's in the sentence, only return for the first one.
+function findNemo(sentance) {
+    let str = sentance.split(' ');
+    for (let i = 0; i < str.length; i++){
+        if (str[i] == "Nemo") return "I found Nemo at " + (i+1) + "!";
+    }
 
-toStr(77) ➞ "77"
-
-toStr(532) ➞ "532"
+    return "I can\'t find Nemo :(";
+}
