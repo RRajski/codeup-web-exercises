a9b963bb53a648d63d14223f2644545a2ad2acc8
Ronald Rajski <ronald.rajski@gmail.com>
2020-01-05 22:07:06 -0600

    edabit

--- Practice/edabit/edabit.js
+++ Practice/edabit/edabit.js
@@ -4,11 +4,11 @@
 // Going from right to left, the value of the most right bit is 1, now from that every bit to the left will be x2 the
 // value, value of an 8 bit binary numbers are (256, 128, 64, 32, 16, 8, 4, 2, 1).
 
-function binary(decimal) {
-    if (decimal <1024 && decimal >= 0){
-        return parseInt(decimal, 10).toString(2);
-    }
-}
+// function binary(decimal) {
+//     if (decimal <1024 && decimal >= 0){
+//         return parseInt(decimal, 10).toString(2);
+//     }
+// }
 
 // Given a string of letters, how many capital letters are there?
 //
@@ -19,17 +19,17 @@ function binary(decimal) {
 //
 // capitalLetters("mqeytbbjwqemcdrdsyvq") ➞ 0
 
-function capitalLetters(txt) {
-    var count = 0;
-    for(var i =0; i<txt.length;i++)
-    {
-        if(txt[i] === txt[i].toUpperCase())
-        {
-            count ++
-        }
-    }
-    return count
-}
+// function capitalLetters(txt) {
+//     var count = 0;
+//     for(var i =0; i<txt.length;i++)
+//     {
+//         if(txt[i] === txt[i].toUpperCase())
+//         {
+//             count ++
+//         }
+//     }
+//     return count
+// }
 
 // Write a function that returns true if k^k == n for input (n, k).
 //
@@ -42,18 +42,110 @@ function capitalLetters(txt) {
 // kToK(3124, 5) ➞ false
 //
 // kToK(17, 3) ➞ false
-function kToK(n, k) {
-    return Math.pow(k,k) === n
-}
-
-// Create a function to concatenate two integer arrays.
-function concat(arr1, arr2) {
-    return arr1.concat(arr2);
-}
+// function kToK(n, k) {
+//     return Math.pow(k,k) === n
+// }
+//
+// // Create a function to concatenate two integer arrays.
+// function concat(arr1, arr2) {
+//     return arr1.concat(arr2);
+// }
 
     // Examples
-concat([1, 3, 5], [2, 6, 8]) ➞ [1, 3, 5, 2, 6, 8]
+// concat([1, 3, 5], [2, 6, 8]) ➞ [1, 3, 5, 2, 6, 8]
+//
+// concat([7, 8], [10, 9, 1, 1, 2]) ➞ [7, 8, 10, 9, 1, 1, 2]
+//
+// concat([4, 5, 1], [3, 3, 3, 3, 3]) ➞ [4, 5, 1, 3, 3, 3, 3, 3]
 
-concat([7, 8], [10, 9, 1, 1, 2]) ➞ [7, 8, 10, 9, 1, 1, 2]
+// Create a function that checks if the argument is an integer or a string. Return int if it's an integer and
+// str if it's a string.
 
-concat([4, 5, 1], [3, 3, 3, 3, 3]) ➞ [4, 5, 1, 3, 3, 3, 3, 3]
+function intOrStr(param) {
+    if (typeof param === num){
+        return "int";}
+    else if(typeof param === "string"){
+        return "str";
+    }
+}
+
+
+// Imagine you run a website that presents users with different
+//     coding challenges in levels Easy, Medium, and Hard, where users
+// get points for completing challenges. An Easy challenge is worth 5
+// points, a Medium challenge is worth 10 points, and a Hard challenge
+// is worth 20 points.
+//
+//     Create a function that takes in the number of each challenge level
+// a user has played and calculates the user's total number of points.
+// Keep in mind that a user cannot complete negative challenges, so the
+// function should return the string "invalid" if any of the passed
+// parameters are negative.
+
+
+
+function scoreCalculator(easy, med, hard) {
+    var totalEasyPoints = (easy*5);
+    var totalMedPoints = (med*10);
+    var totalHardPoints = (hard*20);
+    var totalNumberOfPoints = totalEasyPoints + totalMedPoints + totalHardPoints;
+    if (easy<0||med<0||hard<0){
+        return "invalid";
+    }else{
+        return (totalNumberOfPoints);
+    }}
+
+console.log(scoreCalculator()
+
+// Write a function that takes two numbers and returns if they should be added, subtracted, multiplied or divided
+// to get 24. If none of the operations can give 24, return null.
+//
+//     Examples
+// operation(15, 9) ➞ "added"
+//
+// operation(26, 2) ➞ "subtracted"
+//
+// operation(11, 11) ➞ null
+// Notes
+// Only integers are used as test input.
+//     Numbers should be added, subtracted, divided or multiplied in the order they appear in the parameters.
+function operation(num1, num2) {
+    if(num1 + num2 == 24){
+        return "added";
+    }
+    else if(num1 - num2 == 24){
+        return "subtracted";
+    }
+    else if(num1 * num2 == 24){
+        return "multiplied";
+    }
+    else if(num1/num2 == 24){
+        return "divided";
+    }
+    else
+        return null;
+
+}
+// Write two functions:
+
+
+//
+//     toInt() : A function to convert a string to an integer.
+// toStr() : A function to convert an integer to a string.
+//     Examples
+function toInt(str) {
+    return parseInt(str)
+}
+
+function toStr(int) {
+    return int.toString()
+
+}
+
+toInt("77") ➞ 77
+
+toInt("532") ➞ 532
+
+toStr(77) ➞ "77"
+
+toStr(532) ➞ "532"
