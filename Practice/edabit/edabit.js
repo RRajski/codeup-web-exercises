d18ed399fbe565c3c66c72a4bb240ddff1977951
Ronald Rajski <ronald.rajski@gmail.com>
2020-01-08 13:27:10 -0600

    afternoon push-need to fine tune this lesson

--- Practice/edabit/edabit.js
+++ Practice/edabit/edabit.js
@@ -198,3 +198,22 @@ function getAbsSum(arr){
     }
     return sum
     }
+// Create a function that takes an integer and returns the factorial of that integer. That is, the integer multiplied by all positive lower integers.
+//
+//     Examples
+// factorial(3) ➞ 6
+//
+// factorial(5) ➞ 120
+//
+// factorial(13) ➞ 6227020800
+// Notes
+// Assume all inputs are greater than or equal to 0.
+function factorial(int) {
+    if (int < 0)
+        return -1;
+    else if (int == 0)
+        return 1;
+    else {
+        return (int * factorial(int - 1));
+    }
+}
